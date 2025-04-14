81b55b7c0d3ed4cc69c30c369a0c5a727bd567e0
Bekoutou <freemanobekoutou@gmail.com>
2025-04-14 13:35:29 +0100

    premier java

---
+++ java.js
@@ -0,0 +1,6 @@
+javascript
+// Gestion de la soumission du formulaire
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Merci pour votre message !");
+});
